cbf8a647ccc2efb807feac71f47d8625cd80e8b6
Don Johnson <rajszabi@gmail.com>
2023-10-11 02:22:53 +0200

    add check data integrity

--- flask-app/app/api_requests.py
+++ flask-app/app/api_requests.py
@@ -2,6 +2,7 @@ import json, base64
 from flask import Blueprint, jsonify
 from database_model import Customer
 from cipher import encrypt
+from checksum import check_integrity
 
 
 api = Blueprint('api', __name__)
@@ -20,15 +21,20 @@ def customers():
         customer_dict['IBAN'] = customer.iban
         customers_list.append(customer_dict)
 
-    cipher_text, secret_key_encrypted, iv_encrypted = encrypt(json.dumps(customers_list).encode('utf-8'))
+    encoded_plaintext = json.dumps(customers_list).encode('utf-8')
+    plaintext_checksum = check_integrity(encoded_plaintext)
+
+    cipher_text, secret_key_encrypted, iv_encrypted = encrypt(encoded_plaintext)
 
     cipher_text_base64 = base64.b64encode(cipher_text).decode('utf-8')
     secret_key_encrypted_base64 = base64.b64encode(secret_key_encrypted).decode('utf-8')
     iv_encrypted_base64 = base64.b64encode(iv_encrypted).decode('utf-8')
+    checksum_base64 = base64.b64encode(plaintext_checksum).decode('utf-8')
 
     response_json = dict()
     response_json['text'] = cipher_text_base64
     response_json['secret_key'] = secret_key_encrypted_base64
     response_json['iv'] = iv_encrypted_base64
+    response_json['checksum'] = checksum_base64
 
     return response_json, 200
--- flask-app/app/routes.py
+++ flask-app/app/routes.py
@@ -2,6 +2,7 @@ import json, base64, requests
 from cryptography.hazmat.primitives import serialization
 from flask import Blueprint, jsonify
 from cipher import decrypt
+from checksum import check_integrity
 
 CLIENT_PRIVATE_KEY_PATH = './.secrets/client_private_key.pem'
 CLIENT_PRIVATE_KEY = ""
@@ -23,10 +24,24 @@ def customers():
     ciphertext_base64 = data['text']
     secret_key_encrypted_base64 = data['secret_key']
     iv_encrypted_base64 = data['iv']
+    checksum_base64 = data['checksum']
 
     ciphertext = base64.b64decode(ciphertext_base64)
     secret_key_encrypted = base64.b64decode(secret_key_encrypted_base64)
     iv_encrypted = base64.b64decode(iv_encrypted_base64)
+    sent_checksum = base64.b64decode(checksum_base64)
 
     decrypted_text = decrypt(ciphertext, secret_key_encrypted, iv_encrypted, CLIENT_PRIVATE_KEY)
-    return json.loads(decrypted_text.decode('utf-8')), 200
+
+    recieved_checksum = check_integrity(decrypted_text)
+
+    data = json.loads(decrypted_text.decode('utf-8'))
+
+    if recieved_checksum != sent_checksum:
+        return "Checksums do not match", 400
+    else:
+        response_json = {
+            "checksum": "Checksum match",
+            "data": data
+        }
+        return jsonify(response_json), 200
