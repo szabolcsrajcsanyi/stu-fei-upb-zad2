f1bc4fe6bdd7a3cbd2f24b31b70b313be9712cc5
AndrejKovacik57 <andrejkovacik57@gmail.com>
2023-11-16 23:21:26 +0100

    pridanie tabuliek, uprava initdb- odstranennie vytvaranie usera

--- flask-app/app/database_model.py
+++ flask-app/app/database_model.py
@@ -1,6 +1,5 @@
 from extensions import db
 
-
 class Customer(db.Model):
     __tablename__ = 'customers'
 
@@ -17,6 +16,18 @@ class Customer(db.Model):
     def __repr__(self):
         return f'<Customer {self.name} {self.surname}>'
 
+class Transaction(db.Model):
+    __tablename__ = 'transactions'
+    id = db.Column(db.Integer, primary_key=True)
+    sum = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+    users = db.relationship('User', secondary='user_transaction_association', back_populates='transactions', cascade='all, delete-orphan')
+
+class UserTransactionAssociation(db.Model):
+    __tablename__ = 'user_transaction_association'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    transaction_id = db.Column(db.Integer, db.ForeignKey('transactions.id'))
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -28,13 +39,18 @@ class User(db.Model):
     salt = db.Column(db.String(255), nullable=False)
     hash_pass = db.Column(db.String(255), nullable=False)
     rsa_public_key = db.Column(db.String(2048), nullable=True)
+    iban = db.Column(db.String(12))
+    account_balance = db.Column(db.Integer,nullable=False)
+    transactions = db.relationship('Transaction', secondary='user_transaction_association', back_populates='users', cascade='all, delete-orphan')
 
-    def __init__(self, firstname, lastname, email, salt, hash_pass):
+    def __init__(self, firstname, lastname, email, salt, hash_pass, iban):
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
         self.salt = salt
         self.hash_pass = hash_pass
+        self.account_balance = 0
+        self.iban = iban
 
     def __repr__(self):
         return f'<User {self.firstname} {self.lastname} {self.email} {self.rsa_public_key}>'
